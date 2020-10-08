6bcc155a72ecdffac0c6bf2e69fa9b2d13096d89
Jason Raimondi <jason@raimondi.us>
2020-10-08 16:41:09 -0700

    refactor: fix circular import, dont use index.ts

--- src/requests/authorization.request.ts
+++ src/requests/authorization.request.ts
@@ -1,7 +1,7 @@
-import { OAuthException } from "..";
 import { OAuthClient } from "../entities/client.entity";
 import { OAuthScope } from "../entities/scope.entity";
 import { OAuthUser } from "../entities/user.entity";
+import { OAuthException } from "../exceptions/oauth.exception";
 import { GrantIdentifier } from "../grants/abstract/grant.interface";
 
 export class AuthorizationRequest {
