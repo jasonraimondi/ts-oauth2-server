d2f6b2a4e04b29384a14b2fb66829f687e82e98c
Jason Raimondi <jason@raimondi.us>
2021-09-11 11:06:49 -0700

    fix(test): update test after removal of barrelsby
    we are no longer exporting unnecessary files, and OAuthException was one of them
    switch test to check for actual exported fields

--- test/unit/index.spec.ts
+++ test/unit/index.spec.ts
@@ -1,10 +1,9 @@
-import { ErrorType, OAuthException } from "../../src/exceptions/oauth.exception";
 import { JwtService } from "../../src/utils/jwt";
 
 it("can use the index.ts file", async () => {
   const module = await import("../../src/index");
 
   expect(module).toBeDefined();
-  expect(new module.OAuthException("test", ErrorType.AccessDenied)).toBeInstanceOf(OAuthException);
+  expect(module.AuthorizationServer).toBeInstanceOf(Function);
   expect(new module.JwtService("test")).toBeInstanceOf(JwtService);
 });
